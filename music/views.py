05941d920439d0f32b81229e021b6bf03f6aecd6
Alex <thatsoalex@gmail.com>
2022-04-25 12:32:50 -0500

    Created songs_list function with GET endpoint

--- music/views.py
+++ music/views.py
@@ -4,6 +4,13 @@ from rest_framework.response import Response
 from .models import Songs
 from rest_framework import status
 from .serializers import SongSerializer
+from music import serializers
 
+@api_view(['GET', 'POST'])
+def songs_list(request):
+    if request.method == 'GET':
+        music = Songs.objects.all()
+        serializer = SongSerializer(music, many=True)
+        return Response(serializer.data)
 
 # Create your views here.
