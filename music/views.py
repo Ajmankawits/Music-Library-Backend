7fc854575ab185a6000c0b4974c3117356323e8d
Alex <thatsoalex@gmail.com>
2022-04-25 13:12:28 -0500

    Made PUT Endpoint for songs_list Function

--- music/views.py
+++ music/views.py
@@ -13,6 +13,11 @@ def songs_list(request):
         music = Songs.objects.all()
         serializer = SongSerializer(music, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 
